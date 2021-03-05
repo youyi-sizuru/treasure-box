785016667e2b90d15d27812fe159545e92309c2d
xzp <xuezhenpu@xiaomai5.com>
2021-03-05 18:23:43 +0800

    颜色配置

---
+++ widget/src/main/java/com/lifefighter/widget/web/ExWebView.kt
@@ -0,0 +1,13 @@
+package com.lifefighter.widget.web
+
+import android.content.Context
+import android.util.AttributeSet
+import android.webkit.WebView
+
+/**
+ * @author xzp
+ * @created on 2021/3/5.
+ */
+class ExWebView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : WebView(context, attrs, defStyleAttr)
