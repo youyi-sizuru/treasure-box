06e14354f9c4f3ad24e18b16dc5c0f465da0d7e0
xzp <xuezhenpu@xiaomai5.com>
2022-02-15 18:09:52 +0800

    ocr优化方法

--- ocr/src/main/java/com/lifefighter/ocr/OcrLibrary.kt
+++ ocr/src/main/java/com/lifefighter/ocr/OcrLibrary.kt
@@ -2,10 +2,34 @@ package com.lifefighter.ocr
 
 import android.content.res.AssetManager
 import android.graphics.Bitmap
+import com.lifefighter.utils.bg
+import com.lifefighter.utils.ui
 
 object OcrLibrary {
+    private var mInited: Boolean = false
+
     init {
         System.loadLibrary("ocrlibrary")
+    }
+
+    suspend fun initLibrary(assetManager: AssetManager) = ui {
+        if (mInited) {
+            return@ui true
+        }
+        val inited = bg {
+            init(assetManager)
+        }
+        mInited = inited
+        return@ui inited
+    }
+
+    suspend fun detectBitmap(bitmap: Bitmap, useGpu: Boolean) = ui {
+        if (mInited.not()) {
+            return@ui null
+        }
+        return@ui bg {
+            detect(bitmap, useGpu)
+        }
     }
 
     external fun init(mgr: AssetManager): Boolean
